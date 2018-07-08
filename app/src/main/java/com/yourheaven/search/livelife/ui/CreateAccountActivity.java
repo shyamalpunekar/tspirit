0181a1fd498279b83950dc68a01883ec9be136ba
shyamalpunekar <shyamal.punekar@gmail.com>
2018-07-07 18:01:42 -0700

    Add account registration page

---
+++ app/src/main/java/com/yourheaven/search/livelife/ui/CreateAccountActivity.java
@@ -0,0 +1,15 @@
+package com.yourheaven.search.livelife.ui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.yourheaven.search.livelife.R;
+
+public class CreateAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+    }
+}
